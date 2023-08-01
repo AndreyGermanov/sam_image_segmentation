b55b173213476ca0a4111a30d479d402e00dd426
Andrey Germanov <andrey@germanov.dev>
2023-08-01 19:18:40 +0300

    Add 'encoder' ONNX model. Remove original Pytorch dependency.

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from flask import Flask, request, jsonify
 from waitress import serve
 from PIL import Image
 import numpy as np
-from segment_anything import SamPredictor, sam_model_registry
 import onnxruntime
+from copy import deepcopy
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
 
 def main():
-    # Run web service in a main thread
     serve(app, port=8080)
 
 
@@ -20,60 +19,92 @@ def root():
 
 
 image_embedding = None
+original_size = [0, 0]
+transform_matrix = []
+target_size = 1024
+input_size = [684, 1024]
 img = None
 @app.route("/set_image", methods=["POST"])
 def set_image():
-    global img,image_embedding
+    global img,image_embedding,original_size,transform_matrix
     img = Image.open(request.files["image"].stream)
     img = np.array(img)
-    sam = sam_model_registry["vit_b"](checkpoint="./sam_vit_b_01ec64.pth")
-    predictor = SamPredictor(sam)
-    predictor.set_image(img)
-    image_embedding = predictor.get_image_embedding().numpy().astype(np.float32)
+    original_size = img.shape
+    scale_x = input_size[1] / img.shape[1]
+    scale_y = input_size[0] / img.shape[0]
+    scale = min(scale_x, scale_y)
+    transform_matrix = np.array(
+        [
+            [scale, 0, 0],
+            [0, scale, 0],
+            [0, 0, 1]
+        ]
+    )
+    cv_image = cv2.warpAffine(img, transform_matrix[:2], (input_size[1], input_size[0]), flags=cv2.INTER_LINEAR)
+    encoder = onnxruntime.InferenceSession("vit_b_encoder_q.onnx")
+    outputs = encoder.run(None, {"input_image": cv_image.astype(np.float32)})
+    image_embedding = outputs[0]
+    print("Image embedding ready.")
     return "OK"
 
 
 @app.route("/segment", methods=["POST"])
 def segment_onnx():
-    global img, image_embedding
-    sam = sam_model_registry["vit_b"](checkpoint="./sam_vit_b_01ec64.pth")
-    predictor = SamPredictor(sam)
-    input_point = np.array([[request.form["x"], request.form["y"]]])
-    input_label = np.array([1])
-    onnx_coord = np.concatenate([input_point, np.array([[0.0, 0.0]])], axis=0)[None, :, :].astype(np.float32)
-    onnx_coord = predictor.transform.apply_coords(onnx_coord, img.shape[:2]).astype(np.float32)
-    onnx_label = np.concatenate([input_label, np.array([-1])], axis=0)[None, :].astype(np.float32)
+    global img, image_embedding, original_size, transform_matrix
+    input_points = np.array([[221, 230]])
+    input_labels = np.array([1])
+    onnx_coord = np.concatenate([input_points, np.array([[0.0, 0.0]])], axis=0)[None, :, :].astype(np.float32)
+    onnx_label = np.concatenate([input_labels, np.array([-1])], axis=0)[None, :].astype(np.float32)
+    scale = target_size * 1.0 / max(original_size[0], original_size[1])
+    newh, neww = original_size[0] * scale, original_size[1] * scale
+    neww = int(neww + 0.5)
+    newh = int(newh + 0.5)
+    coords = deepcopy(onnx_coord).astype(float)
+    coords[..., 0] = coords[..., 0] * (neww / original_size[1])
+    coords[..., 1] = coords[..., 1] * (newh / original_size[0])
+    onnx_coord = coords.astype(np.float32)
+    onnx_coord = np.concatenate([onnx_coord, np.ones((1, onnx_coord.shape[1], 1), dtype=np.float32)], axis=2)
+    onnx_coord = np.matmul(onnx_coord, transform_matrix.T)
+    onnx_coord = onnx_coord[:, :, :2].astype(np.float32)
     onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
     onnx_has_mask_input = np.zeros(1, dtype=np.float32)
-    ort_inputs = {
+    decoder_inputs = {
         "image_embeddings": image_embedding,
         "point_coords": onnx_coord,
         "point_labels": onnx_label,
         "mask_input": onnx_mask_input,
         "has_mask_input": onnx_has_mask_input,
-        "orig_im_size": np.array(img.shape[:2], dtype=np.float32)
+        "orig_im_size": np.array(input_size, dtype=np.float32),
     }
-    ort_session = onnxruntime.InferenceSession("vit_b.onnx")
-    masks, _, low_res_logits = ort_session.run(None, ort_inputs)
-    mask = (masks[0, 0] > 0).astype('uint8')*255
+    model = onnxruntime.InferenceSession("vit_b.onnx")
+    masks, probs, _ = model.run(None, decoder_inputs)
+    inv_transform_matrix = np.linalg.inv(transform_matrix)
+    transformed_masks = transform_masks(
+        masks, original_size, inv_transform_matrix
+    )
+    masks = transformed_masks[0]
+    mask = (masks[probs.argmax()] > 0).astype('uint8') * 255
     contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contour = [[int(contour[0][0]), int(contour[0][1])] for contour in contours[0][0]]
+    print("Contour calculated.")
     return jsonify({"status": "ok", "contour": contour})
 
 
-@app.route("/segment_pytorch", methods=["POST"])
-def segment_pytorch():
-    img = Image.open(request.files["image"].stream)
-    img = np.array(img)
-    sam = sam_model_registry["vit_b"](checkpoint="./sam_vit_b_01ec64.pth")
-    predictor = SamPredictor(sam)
-    predictor.set_image(img)
-    masks, probs, _ = predictor.predict(point_coords=np.array([[request.form["x"], request.form["y"]]]), point_labels=np.array([1]),
-                                    multimask_output=True)
-    mask = masks[probs.argmax()].astype('uint8')*255
-    contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contour = [[int(contour[0][0]), int(contour[0][1])] for contour in contours[0][0]]
-    return jsonify({"status": "ok", "contour": contour})
+def transform_masks(masks, original_size, transform_matrix):
+    output_masks = []
+    for batch in range(masks.shape[0]):
+        batch_masks = []
+        for mask_id in range(masks.shape[1]):
+            mask = masks[batch, mask_id]
+            mask = cv2.warpAffine(
+                mask,
+                transform_matrix[:2],
+                (original_size[1], original_size[0]),
+                flags=cv2.INTER_LINEAR,
+            )
+            batch_masks.append(mask)
+        output_masks.append(batch_masks)
+    return np.array(output_masks)
 
 
 main()
